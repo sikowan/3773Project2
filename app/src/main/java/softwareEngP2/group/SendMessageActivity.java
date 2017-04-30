13593f2b7e2b155d2f67db11db498084ccc8132e
jawoodal <jawoodal@hotmail.com>
2017-04-29 22:47:06 -0500

    added activity classes and various layouts. buttons on main menu work.

---
+++ app/src/main/java/softwareEngP2/group/SendMessageActivity.java
@@ -0,0 +1,19 @@
+package softwareEngP2.group;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class SendMessageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_send_message);
+
+        Button mSendButton = (Button) findViewById(R.id.sendButton);
+
+    }
+}
